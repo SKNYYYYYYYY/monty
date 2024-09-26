bb865b94d6feae1c46f5456dbf49aa9083b415b8
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-26 19:05:04 +0300

    task 0

--- 1-opcodes.c
+++ 1-opcodes.c
@@ -72,16 +72,14 @@ void pall(stack_t **stack, unsigned int line_number)
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	(void) stack;
-
-	if (arguments->head == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		dprintf(2, "L%d: can't pint, stack empty\n", line_number);
 		free_all();
 		exit(EXIT_FAILURE);
 	}
 
-	printf("%d\n", arguments->head->n);
+	printf("%d\n", (*stack)->n);
 }
 
 
